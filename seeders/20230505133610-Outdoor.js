b78fe820f80b8dd7c08f35a7b62eae8bee6c84cb
Shineka Solomon <neka.solomon@gmail.com>
2023-05-05 15:39:20 -0400

    corrected syntx errors on Tropical and Outdoor tables

--- seeders/20230505133610-Outdoor.js
+++ seeders/20230505133610-Outdoor.js
@@ -23,12 +23,11 @@ module.exports = {
         ],
         recipe: "1. Mix the Neem oil and liquid dish soap together in a small bowl.\n2. Add the mixture to a quart-sized spray bottle filled with water.\n3. Shake well to combine all the ingredients.\n4. Your pesticide spray is now ready to use.",
         image: ""
-      }
-    ]);
+      },
     
-
+    
     {
-      name: "Garlic and chili pepper spray:",
+      name: "Garlic and chili pepper spray",
       ingredients: [
         "1 head of garlic",
         "2-3 hot chili peppers",
@@ -37,10 +36,10 @@ module.exports = {
       ], 
       recipe: "Peel and crush the garlic and chop the chili peppers.\nAdd the garlic and chili peppers to a quart-sized jar and fill with water.\nLet the mixture steep for 24 hours.\nStrain the mixture and add the liquid dish soap.\nPour the mixture into a spray bottle.\nInstructions for use:\n1. Spray the pesticide directly onto the affected plants, making sure to cover all parts of the plant.\n2. Repeat the application every 7-14 days until the pests are gone.\n3. Avoid spraying the pesticide during the heat of the day, as this can cause the leaves to burn.",
       image: ""
-    }
+    },
     
       {
-      name: "Vegetable Oil Sray:", 
+      name: "Vegetable Oil Sray", 
       ingredients: [
         "2 tablespoons of vegetable oil",
         "1 tablespoon of liquid dish soap",
@@ -49,13 +48,16 @@ module.exports = {
       recipe: "Mix the vegetable oil and liquid dish soap together in a small bowl.\nAdd the mixture to a quart-sized spray bottle filled with water.\nShake well to combine all the ingredients.\nYour pesticide spray is now ready to use.\nInstructions for use:\n1.Spray the pesticide directly onto the affected plants, making sure to cover all parts of the plant.\n2.Repeat the application every 7-14 days until the pests are gone.\nAvoid spraying the pesticide during the heat of the day", 
       image: ""}
 
-      {name: "Soft-bodies Pest Spray", 
+      {
+        name: "Soft-bodies Pest Spray", 
       ingredients: [
         "1 quart of water",
         "1 teaspoon of liquid dish soap",
         "1 teaspoon of baking soda",
         "1 teaspoon of vegetable oil",
-      ], recipe: " In a spray bottle, mix the water and liquid dish soap together.\nAdd the baking soda and stir until dissolved.\nAdd the vegetable oil and shake the bottle vigorously to combine all the ingredients.\nYour pesticide spray is now ready to use.\nInstructions for use:\n1. Spray the pesticide directly onto the affected plants, making sure to cover all parts of the plant, including the underside of the leaves.\n2. Repeat the application every few days until the pests are gone.\n3. Avoid spraying the pesticide during the heat of the day, as this can cause the leaves to burn.\n4.Always test a small area of the plant first to make sure it does not have an adverse reaction to the spray.\n5. Store any leftover pesticide in a cool, dry place, out of reach of children and pets.", image: ""}
+      ], recipe: " In a spray bottle, mix the water and liquid dish soap together.\nAdd the baking soda and stir until dissolved.\nAdd the vegetable oil and shake the bottle vigorously to combine all the ingredients.\nYour pesticide spray is now ready to use.\nInstructions for use:\n1. Spray the pesticide directly onto the affected plants, making sure to cover all parts of the plant, including the underside of the leaves.\n2. Repeat the application every few days until the pests are gone.\n3. Avoid spraying the pesticide during the heat of the day, as this can cause the leaves to burn.\n4.Always test a small area of the plant first to make sure it does not have an adverse reaction to the spray.\n5. Store any leftover pesticide in a cool, dry place, out of reach of children and pets.", image: ""
+    },
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
