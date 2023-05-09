3dcc0e97a51b82dd58e14899cbb48ede956f4645
Nati Worku <natiworku1971@gmail.com>
2023-05-09 10:31:36 -0400

    chat name search

--- routes/search.js
+++ routes/search.js
@@ -11,5 +11,42 @@ router.get('/', async (req, res, next) => {
     let results = { multiPurpose, detergent, topical, outdoor }
     res.status(200).json(results)
 });
-
+router.get('/:search', async (req, res, next) => {
+    try {
+        let { search } = req.params; // extract query parameter 'q' from the request
+        let multiPurpose = await MultiPurpose.findAll({
+            where: {
+                name: {
+                    [Op.like]: `%${search}%` // use the like operator to search for records that match the query
+                }
+            }
+        });
+        let detergent = await Detergent.findAll({
+            where: {
+                name: {
+                    [Op.like]: `%${search}%`
+                }
+            }
+        });
+        let topical = await Topical.findAll({
+            where: {
+                name: {
+                    [Op.like]: `%${search}%`
+                }
+            }
+        });
+        let outdoor = await Outdoor.findAll({
+            where: {
+                name: {
+                    [Op.like]: `%${search}%`
+                }
+            }
+        });
+        let results = { multiPurpose, detergent, topical, outdoor };
+        res.status(200).json(results);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error fetching data from database" });
+    }
+})
 module.exports = router;
